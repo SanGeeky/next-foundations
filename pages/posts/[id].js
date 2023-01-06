43628a84675452c289a294410c6257e6f0f907c8
David Pinchao <sangeeky@outlook.com>
2023-01-06 09:58:38 -0500

    refactor API implementation

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,28 +1,28 @@
 import Head from 'next/head';
-import { getAllPostIds, getPostData, getArticlesIds, getArticle } from '../../lib/posts';
+import { getArticleIds, getArticle } from '../../lib/posts';
 import Layout from '../../components/layout';
 import Date from '../../components/date';
 import utilStyles from '../../styles/utils.module.css'
 
-export default function Post({ postData }) {
+export default function Post({ articleData }) {
   return (
     <Layout>
       <Head>
-        <title>{postData.title}</title>
+        <title>{articleData.title}</title>
       </Head>
       <article>
-        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <h1 className={utilStyles.headingXl}>{articleData.title}</h1>
         <div className={utilStyles.lightText}>
-          <Date dateString={postData.date} />
+          <Date dateString={articleData.date} />
         </div>
-        <div dangerouslySetInnerHTML={{ __html: postData.content }} />
+        <div dangerouslySetInnerHTML={{ __html: articleData.content }} />
       </article>
     </Layout>
   );
 }
 
 export async function getStaticPaths() {
-  const paths = await getArticlesIds();
+  const paths = await getArticleIds();
   return {
     paths,
     fallback: false,
@@ -30,10 +30,10 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const postData = await getArticle(params.id);
+  const articleData = await getArticle(params.id);
   return {
     props: {
-      postData,
+      articleData,
     },
   };
 }
